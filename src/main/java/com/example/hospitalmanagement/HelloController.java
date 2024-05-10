a8127449ff7dd4b5eea62858e4b362f5fe9980e6
shahriar-kabir <shahriarkabir280@gmail.com>
2024-05-10 17:22:55 +0600

    do commit

--- src/main/java/com/example/hospitalmanagement/HelloController.java
+++ src/main/java/com/example/hospitalmanagement/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-        System.out.println("KOKO");
+
     }
 }
