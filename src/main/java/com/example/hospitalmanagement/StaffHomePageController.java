28861bee78d2c060d390d5ae54d77cf9fbc16537
nanden <mdnurannahadi@gmail.com>
2024-05-16 22:40:53 +0600

    change scene transition

--- src/main/java/com/example/hospitalmanagement/StaffHomePageController.java
+++ src/main/java/com/example/hospitalmanagement/StaffHomePageController.java
@@ -1,4 +1,88 @@
 package com.example.hospitalmanagement;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class StaffHomePageController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private Button button_Update;
+
+    @FXML
+    private Button button_attendance;
+
+    @FXML
+    private Button button_logout;
+
+    @FXML
+    private TextField dobField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField shiftField;
+
+    @FXML
+    private TextField shiftText;
+
+    @FXML
+    private TextField usernameField;
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    public void setProfile(String user, String pass) throws SQLException {
+        try {
+            connect = HospitalManagementDatabase.connectDB();
+            prepare = connect.prepareStatement("SELECT * FROM staff WHERE username = ? AND password = ?");
+            prepare.setString(1, user);
+            prepare.setString(2, pass);
+            result = prepare.executeQuery();
+
+            while (result.next()) {
+
+                nameField.setText(result.getString("fullname"));
+                usernameField.setText(result.getString("username"));
+                emailField.setText(result.getString("email"));
+                phoneField.setText(result.getString("phonenumber"));
+                dobField.setText(result.getString("date_of_birth"));
+                addressField.setText(result.getString("address"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close resources
+            if (result != null) {
+                result.close();
+            }
+            if (prepare != null) {
+                prepare.close();
+            }
+            if (connect != null) {
+                connect.close();
+            }
+        }
+    }
 }
