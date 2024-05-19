6ba7d8d67a5c81d010c40d641893d340048f5994
nanden <mdnurannahadi@gmail.com>
2024-05-19 20:23:25 +0600

    updateinfo for doctor fixed

--- src/main/java/com/example/hospitalmanagement/HospitalManagementDatabase.java
+++ src/main/java/com/example/hospitalmanagement/HospitalManagementDatabase.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class HospitalManagementDatabase {
       public static  Connection connectDB(){
             try{
-                  Connection connect =DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement","root","sh!m@nt02024");
+                  Connection connect =DriverManager.getConnection("jdbc:mysql://localhost:3306/hospitalmanagement","root","ROOT#passw0rd");
                   return connect;
             }
             catch (Exception e){
